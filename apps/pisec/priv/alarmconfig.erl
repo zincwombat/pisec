e3c82e053a3f9597171c9718d71a03a867323267
Tony Hobbins <mail@tonyhobbins.com>
2014-01-31 16:58:40 +1100

    moved alarmconfig.erl to priv directory

---
+++ apps/pisec/priv/alarmconfig.erl
@@ -0,0 +1,10 @@
+{ports,[
+	{1,"Front Door Sensor",enabled,0},
+	{2,"Rear Door Sensor",enabled,0},
+	{3,"Hallway PIR Sensor",enabled,0},
+	{4,"Living Room PIR Sensor",enabled,0},
+	{5,"Sensor 5",disabled,0},
+	{6,"Sensor 6",disabled,0},
+	{7,"Sensor 7",disabled,0},
+	{8,"Sensor 8",disabled,0}
+]}.
