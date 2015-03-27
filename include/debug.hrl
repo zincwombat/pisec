399ebb9fcc69301b00cef678f13d055de2ab3526
Tony Hobbins <mail@tonyhobbins.com>
2015-03-28 10:05:19 +1100

    changed to use lager errors deleted reference to esn_kernel

--- include/debug.hrl
+++ include/debug.hrl
@@ -1,22 +1,20 @@
--define(TRACE_DEBUG,		0).
--define(TRACE_INFO,			1).
--define(TRACE_WARN,			2).
--define(TRACE_CRITICAL,		3).
-
--define(tracedebug,			put(trace_level,?TRACE_DEBUG)).
--define(traceinfo,			put(trace_level,?TRACE_INFO)).
+-define(TRACE_DEBUG,	0).
+-define(TRACE_INFO,		1).
+-define(TRACE_WARN,		2).
+-define(TRACE_CRITICAL,	3).
+-define(TRACE_DEFAULT,	1).
 
 -ifndef(TRACE_LEVEL).
 -define(TRACE_LEVEL,?TRACE_INFO).
 -endif.
 
-
--define(trace(Str),			syslogger:trace(?MODULE,{{line,?LINE},Str})).
--define(dbug(Str),			syslogger:debug(?MODULE,{{line,?LINE},Str})).
--define(info(Str),			syslogger:info(?MODULE,{{line,?LINE},Str})).
--define(warn(Str),			syslogger:warn(?MODULE,{{line,?LINE},Str})).
--define(critical(Str),		syslogger:critical(?MODULE,{{line,?LINE},Str})).
--define(tracelevel(Lvl),	syslogger:add(?MODULE,Lvl)).
-
--define(location,			{location,{module,?MODULE},{line,?LINE}}).
--define(error(E),			{error,{?location,{detail,E}}}).
+-define(DEBUG, 0).
+-define(LAGER, 1).
+-ifdef(LAGER).
+-define(trace(Str),		lager:trace("~p",[Str])).
+-define(dbug(Str),		lager:debug("~p",[Str])).
+-define(info(Str),		lager:info("~p",[Str])).
+-define(error(Str),		lager:error("~p",[Str])).
+-define(warn(Str),		lager:warning("~p",[Str])).
+-define(critical(Str),	lager:critical("~p",[Str])).
+-endif.
