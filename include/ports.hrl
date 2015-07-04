5ef957ffcb2ab30da5a7783eeac4b75c41172400
zincwombat <mail@tonyhobbins.com>
2015-07-04 11:49:06 +1000

    updated

--- include/ports.hrl
+++ include/ports.hrl
@@ -4,7 +4,7 @@
 -define(is_uint32(T), 	(((T) band (bnot 16#ffffffff)) =:=  0)).
 
 %% INPUT PORTS
--define(PORTS,			[0,1,2,3,4,5,6,7]).
+-define(PORTS,			[7,6,5,4,3,2,1,0]).
 -define(is_portnum(T),	(T >= 0) andalso (T =< 7)). 
 -define(is_control(T),	(T >= 0) andalso (T =< 2)). 
 -define(is_sensor(T),	(T >= 3) andalso (T =< 7)).
