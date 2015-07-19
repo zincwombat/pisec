216347f9aabdeb064fa07450b26c21dc07d1f5d3
zincwombat <mail@tonyhobbins.com>
2015-07-19 12:46:07 +1000

    changed output_manager

--- src/output_manager.erl
+++ src/output_manager.erl
@@ -1,272 +1,159 @@
 -module(output_manager).
 -behaviour(gen_server).
 
+-export([start_link/0,
+         stop/0]).
+
 -include("debug.hrl").
 -include("ports.hrl").
 -include("alarm.hrl").
 
--export([
-	start/0,
-	stop/0
-]).
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
 
--export([
-	init/1,
-	handle_call/3,
-	handle_cast/2,
-	handle_info/2,
-	terminate/2,
-	code_change/3
-]).
+% API
 
--export([
-	state/0,
-	get/1,
-	set/1,
-	cmd/2,
-	clear/1,
-	clear/0,
-	flash/1,
-	flash/2
-]).
-
--export ([siren/1]).
+-export ([register/2]).
+-export ([unregister/0]).
+-export ([show/0]).
+-export ([notify/3]).
+-export ([getState/0]).
+-export ([getState/1]).
 
 
--record(state, {
-	otab,
-	slow_tm_int,
-	normal_tm_int,
-	fast_tm_int,
-	slow_set=0,
-	normal_set=0,
-	fast_set=0
-}).
 
-start()->
+-record (state, {itab}).
+
+%==============================================================================
+% API
+%==============================================================================
+
+start_link() ->
 	gen_server:start_link({local,?MODULE},?MODULE,[],[]).
 
-stop()->
+stop() ->
 	gen_server:call(?MODULE,stop).
 
-state()->
+state(Pid) ->
 	gen_server:call(?MODULE,state).
 
-cmd(on,OutputPort)->
-	set(OutputPort);
+register(Port,Label)->
+	gen_server:call(?MODULE,{register,Port,self(),Label}).
 
-cmd(off,OutputPort)->
-	clear(OutputPort);
+unregister()->
+	gen_server:call(?MODULE,{unregister,self()}).
 
-cmd(Other,_)->
-	{error,{badarg,Other}}.
+getState()->
+	gen_server:call(?MODULE,getState).
 
+getState(Port)->
+	gen_server:call(?MODULE,{getState,Port}).
 
-set(OutputPort)->
-	gen_server:call(?MODULE,{set,OutputPort}).
+notify(PortNumber,NewValue,OldValue)->
+	gen_server:cast(?MODULE,{notify,PortNumber,NewValue,OldValue}).
 
-get(OutputPort)->
-	gen_server:call(?MODULE,{get,OutputPort}).
-
-clear(OutputPort)->
-	gen_server:call(?MODULE,{clear,OutputPort}).
-
-flash(OutputPort)->
-	flash(OutputPort,normal).
-
-flash(OutputPort,Speed)->
-	gen_server:call(?MODULE,{flash,OutputPort,Speed}).
-
-clear()->
-	gen_server:call(?MODULE,clear).
-
-
-%========================================================================================
-% Utility functions
-%========================================================================================
-
-siren(Control) when ?is_onoff(Control)->
-	Outputs=config:get(outputs),
-	case lists:keyfind(siren,2,Outputs) of
-		{Port,_,_,_}->
-			cmd(Control,Port);
-
-		false->
-			{error,badarg}
-	end;
-
-siren(Control)->
-	{error,{badarg,Control}}.
-
-
-
-
+%==============================================================================
+% callback functions
+%==============================================================================
 
 init([])->
-	State=#state{},
-	?info({starting,{pid,self()}}),
+	?info({pid,self()}),
 	process_flag(trap_exit,true),
+	ITab=ets:new(itab,[set,public]),
+	State=#state{itab=ITab},
+	{ok,State}.
 
-	piface2:write_output(0),
-
-	%% this stores the flash state of each LED
-
-	OTab=ets:new(otab,[set,public,named_table]),
-
-	SlowFlash=config:get(output_flash_slow,?FLASH_SLOW),
-	NormalFlash=config:get(output_flash_normal,?FLASH_NORMAL),
-	FastFlash=config:get(output_flash_fast,?FLASH_FAST),
-
-	OutputPorts=config:get(outputs),
-
-	%% process the initial state of each output port
-
-	lists:map(fun(Z)->i_handleOutput(Z) end,OutputPorts),
-
-	%% start the flash timers
-
-	T1=erlang:start_timer(SlowFlash,self(),{tm,slow}),
-	T2=erlang:start_timer(NormalFlash,self(),{tm,normal}),
-	T3=erlang:start_timer(FastFlash,self(),{tm,fast}),
-	
-	{ok,State#state{otab=OTab,
-			slow_tm_int=SlowFlash,
-			normal_tm_int=NormalFlash,
-			fast_tm_int=FastFlash}}.
-
-% {0,siren,"Siren (Relay 1)",true,off,power}
-
-i_handleOutput(O={PortNum,_,_,_,on,_})->
-	i_setPort(PortNum);
-
-i_handleOutput(O={PortNum,_,_,_,off,_})->
-	i_clearPort(PortNum).
-
-i_clearPort(PortNum)->
-	CurrentOutputs=piface2:read_output(),
-	NewOutputs=CurrentOutputs band bnot (1 bsl (PortNum)),
-	Reply=piface2:write_output(NewOutputs).
-
-i_setPort(PortNum)->
-	CurrentOutputs=piface2:read_output(),
-	NewOutputs=CurrentOutputs bor (1 bsl (PortNum)),
-	Reply=piface2:write_output(NewOutputs).
-
-is(Speed,{_Port,Speed})->
-	true;
-
-is(_,_)->
-	false.
-
-handle_call(stop,_from,State)->
+handle_call(stop,_From,State)->
 	{stop,normal,ok,State};
 
 handle_call(state,_From,State)->
 	{reply,{ok,State},State};
 
-handle_call({set,OutputPort},_From,State) when ?is_oport(OutputPort)->
-	ets:delete(State#state.otab,OutputPort),
-	CurrentOutputs=piface2:read_output(),
-	NewOutputs=CurrentOutputs bor (1 bsl (OutputPort)),
-	Reply=piface2:write_output(NewOutputs),
+handle_call({register,Port,Pid,Label},_From,State=#state{itab=ITab}) when is_pid(Pid)->
+    ets:insert(ITab,{Port,Pid,Label}),
+    ?info({added_handler,{port,Port}}),
+    link(Pid),	
+   	{reply,ok,State};
+
+handle_call({unregister,Pid},_From,State=#state{itab=ITab}) when is_pid(Pid)->
+    ets:match_delete(ITab,{'_',Pid,'_'}),
+    unlink(Pid),	
+   	{reply,ok,State};
+
+handle_call(getState,_From,State=#state{itab=ITab})->
+    Handlers=ets:tab2list(ITab),
+    Reply=lists:map(fun(Z)->input_handler:getState(element(2,Z)) end,Handlers),
+   	{reply,Reply,State};
+
+
+% 	allow both port labels (atoms) and integers to be used 
+
+handle_call({getState,Port},_From,State=#state{itab=ITab}) when ?is_portnum(Port)->
+    Reply=
+    case ets:lookup(ITab,Port) of
+    	[{Port,Pid,_}]->
+    		input_handler:getState(Pid);
+    	_->
+    		?error({badarg,{port,Port}}),
+    		[]
+    end,
+   	{reply,Reply,State};
+
+handle_call({getState,Label},_From,State=#state{itab=ITab}) when is_atom(Label)->
+	Handlers=ets:tab2list(ITab),
+	Reply=
+	case lists:keyfind(Label,3,Handlers) of
+		{Port,Pid,Label}->
+			input_handler:getState(Pid);
+		_->
+			?error({badarg,{label,Label}}),
+			[]
+	end,
 	{reply,Reply,State};
 
-handle_call(clear,_From,State)->
-	ets:delete_all_objects(State#state.otab),
-	Reply=piface2:write_output(0),
-	{reply,Reply,State#state{slow_set=0,normal_set=0,fast_set=0}};
-
-handle_call({clear,OutputPort},_From,State) when ?is_oport(OutputPort)->
-	i_clearPort(OutputPort),
-	ets:delete(State#state.otab,OutputPort),
-	TL=ets:tab2list(State#state.otab),
-	Fast=lists:filter(fun(Z)->is(fast,Z) end,TL),
-	Normal=lists:filter(fun(Z)->is(normal,Z) end,TL),
-	Slow=lists:filter(fun(Z)->is(slow,Z) end,TL),
-	{reply,{Slow,Normal,Fast},State#state{slow_set=buildMask(Slow),
-					      				  normal_set=buildMask(Normal),
-					      				  fast_set=buildMask(Fast)}};
-
-handle_call({flash,OutputPort,off},_From,State) when ?is_led(OutputPort)->
-	ets:delete(State#state.otab,OutputPort),
-	TL=ets:tab2list(State#state.otab),
-	Fast=lists:filter(fun(Z)->is(fast,Z) end,TL),
-	Normal=lists:filter(fun(Z)->is(normal,Z) end,TL),
-	Slow=lists:filter(fun(Z)->is(slow,Z) end,TL),
-	i_clearPort(OutputPort),
-	{reply,{Slow,Normal,Fast},State#state{slow_set=buildMask(Slow),
-					      				  normal_set=buildMask(Normal),
-					      				  fast_set=buildMask(Fast)}};
-
-handle_call({flash,OutputPort,Speed},_From,State) when ?is_led(OutputPort),?is_speed(Speed)->
-	ets:insert(State#state.otab,{OutputPort,Speed}),
-	TL=ets:tab2list(State#state.otab),
-	Fast=lists:filter(fun(Z)->is(fast,Z) end,TL),
-	Normal=lists:filter(fun(Z)->is(normal,Z) end,TL),
-	Slow=lists:filter(fun(Z)->is(slow,Z) end,TL),
-	i_setPort(OutputPort),
-	{reply,{Slow,Normal,Fast},State#state{slow_set=buildMask(Slow),
-					      				  normal_set=buildMask(Normal),
-					      				  fast_set=buildMask(Fast)}};
 
 handle_call(Msg,From,State)->
-	?warn({unhandled_call,{msg,Msg},{from,From}}),
-	{reply,ignored,State}.
+	Unhandled={unhandled_call,{msg,Msg},{from,From}},
+	?warn(Unhandled),
+	{reply,Unhandled,State}.
+
+handle_cast({notify,PortNumber,NewValue,OldValue},State=#state{itab=ITab})->
+	?info({port,PortNumber,new,NewValue,old,OldValue}),
+	% dispatch the message to the handler
+	case ets:lookup(ITab, PortNumber) of
+		[{PortNumber, Pid, _}]->
+			Pid ! {stateChange,NewValue,OldValue};
+		_->
+			% no registered handler
+			?warn({no_handler,{port,PortNumber}})
+	end,
+	{noreply,State};
 
 handle_cast(Msg,State)->
+	Unhandled={unhandled_cast,{msg,Msg}},
+	?warn(Unhandled),
 	{noreply,State}.
 
-handle_info({timeout,_TRef,{tm,slow}},State=#state{slow_tm_int=Slow,slow_set=S})->
-
-	CurrentOutputs=piface2:read_output(),
-	NewOutputs=CurrentOutputs bxor S,
-	Reply=piface2:write_output(NewOutputs),
-
-	erlang:start_timer(Slow,self(),{tm,slow}),
-	{noreply,State};
-
-handle_info({timeout,_TRef,{tm,normal}},State=#state{normal_tm_int=Normal,normal_set=S})->
-
-	CurrentOutputs=piface2:read_output(),
-	NewOutputs=CurrentOutputs bxor S,
-	Reply=piface2:write_output(NewOutputs),
-
-	erlang:start_timer(Normal,self(),{tm,normal}),
-	{noreply,State};
-
-handle_info({timeout,_TRef,{tm,fast}},State=#state{fast_tm_int=Fast,fast_set=S})->
-
-	CurrentOutputs=piface2:read_output(),
-	NewOutputs=CurrentOutputs bxor S,
-	Reply=piface2:write_output(NewOutputs),
-
-	erlang:start_timer(Fast,self(),{tm,fast}),
-	{noreply,State};
-
-handle_info({timeout,_TRef,{tm,Speed}},State=#state{})->
+handle_info(Msg={'EXIT',Pid,Reason},State=#state{itab=ITab})->
+	?warn(Msg),
+	% if this is an input port handler, delete it from itab
+	ets:match_delete(ITab,{'_',Pid,'_'}),
 	{noreply,State};
 
 handle_info(Msg,State)->
-	?warn({unhandled_info,{msg,Msg}}),
+	Unhandled={unhandled_info,{msg,Msg}},
+	?warn(Unhandled),
 	{noreply,State}.
 
 code_change(_OldVsn,Ctx,_Extra) ->
 	{ok,Ctx}.	
 
-terminate(Reason,_State)->
+terminate(Reason,#state{})->
 	?info({terminating,Reason}),
-	ok.
-
-%%========================================================================================
-%% Utility functions
-%%========================================================================================
-
-
-buildMask(S) when is_list(S)->
-	lists:foldl(fun(Z,Byte)->Byte bor (1 bsl (element(1,Z))) end,0,S);
-
-buildMask(_)->
-	0.
-
+	ok. 
 
 
