9b102ad1557943181751bb6143820e9e6a16ee09
zincwombat <mail@tonyhobbins.com>
2015-07-19 17:53:37 +1000

    added timer

--- include/ports.hrl
+++ include/ports.hrl
@@ -1,20 +1,21 @@
--record(portRec,		{port}).
--define(is_uint8(T), 	(((T) band (bnot 16#ff)) =:=  0)).
--define(is_uint16(T), 	(((T) band (bnot 16#ffff)) =:=  0)).
--define(is_uint32(T), 	(((T) band (bnot 16#ffffffff)) =:=  0)).
+-record(portRec,			{port}).
+-define(is_uint8(T), 		(((T) band (bnot 16#ff)) =:=  0)).
+-define(is_uint16(T), 		(((T) band (bnot 16#ffff)) =:=  0)).
+-define(is_uint32(T), 		(((T) band (bnot 16#ffffffff)) =:=  0)).
 
 %% INPUT PORTS
--define(PORTS,			[7,6,5,4,3,2,1,0]).
--define(is_portnum(T),	(T >= 0) andalso (T =< 7)). 
--define(is_control(T),	(T >= 0) andalso (T =< 2)). 
--define(is_sensor(T),	(T >= 3) andalso (T =< 7)).
+-define(PORTS,				[7,6,5,4,3,2,1,0]).
+-define(is_portnum(T),		(T >= 0) andalso (T =< 7)). 
+-define(is_control(T),		(T >= 0) andalso (T =< 2)). 
+-define(is_sensor(T),		(T >= 3) andalso (T =< 7)).
 
 %% OUTPUT PORTS
--define(is_oport(T),	(T >= 2) andalso (T =< 7)).
--define(is_relay(T),	(T >= 0) andalso (T =< 1)). 
--define(is_led(T),		(T >= 2) andalso (T =< 7)). 
+-define(is_oport(T),		(T >= 2) andalso (T =< 7)).
+-define(is_relay(T),		(T >= 0) andalso (T =< 1)). 
+-define(is_led(T),			(T >= 2) andalso (T =< 7)). 
 
--define(is_onoff(T),	(T == on) orelse (T == off)).
+-define(is_onoff(T),		(T == on) orelse (T == off)).
 
--define(is_speed(T),	(T == fast) orelse (T == normal) orelse (T == slow) orelse (T == off)).
+-define(is_speed(T),		(T == fast) orelse (T == normal) orelse (T == slow) orelse (T == off)).
+-define(is_flash_allowed(T),(T >= 10)).
 
--- src/led_handler.erl
+++ src/led_handler.erl
@@ -19,8 +19,9 @@
 
 -export ([on/1]).
 -export ([off/1]).
+-export ([flash/2]).
 
--record (state, {port,ledStatus,label,desc}).
+-record (state, {port,ledStatus,label,desc,timer,timer_intv}).
 
 %==============================================================================
 % API
@@ -41,6 +42,9 @@ on(Pid) ->
 off(Pid) ->
 	gen_server:call(Pid,off).
 
+flash(Pid,Speed)->
+	gen_server:call(Pid,{flash,Speed}).
+
 %==============================================================================
 % callback functions
 %==============================================================================
@@ -60,17 +64,27 @@ handle_call(state,_From,State)->
 
 handle_call(on,_From,State=#state{port=Port})->
 	% turn led on
-	CurrentOutputs=piface2:read_output(),
-	NewOutputs=CurrentOutputs bor (1 bsl (Port)),
-	Reply=piface2:write_output(NewOutputs),
+	i_on(Port),
 	{reply,ok,State#state{ledStatus=on}};
 
 handle_call(off,_From,State=#state{port=Port})->
 	% turn led off
-	CurrentOutputs=piface2:read_output(),
-	NewOutputs=CurrentOutputs band bnot (1 bsl (Port)),
-	Reply=piface2:write_output(NewOutputs),
+	i_off(Port),
 	{reply,ok,State#state{ledStatus=off}};
+
+handle_call({flash,off},_From,State=#state{port=Port,timer=TRef}) when is_reference(TRef)->
+	erlang:cancel_timer(TRef),
+	i_off(Port),
+	{reply,ok,State#state{timer=null,ledStatus=off,timer_intv=null}};
+
+handle_call({flash,Speed},_From,State=#state{port=Port,timer=TRef}) when is_reference(TRef)->
+	erlang:cancel_timer(TRef),
+	NewTRef=erlang:start_timer(Speed,self(),{fl_timeout,Speed}),
+	{reply,ok,State#state{timer=NewTRef,ledStatus={flash,Speed},timer_intv=Speed}};
+
+handle_call({flash,Speed},_From,State=#state{port=Port,timer=TRef})->
+	NewTRef=erlang:start_timer(Speed,self(),{fl_timeout,Speed}),
+	{reply,ok,State#state{timer=NewTRef,ledStatus={flash,Speed},timer_intv=Speed}};
 
 handle_call(Msg,From,State)->
 	Unhandled={unhandled_call,{msg,Msg},{from,From}},
@@ -81,6 +95,15 @@ handle_cast(Msg,State)->
 	Unhandled={unhandled_cast,{msg,Msg}},
 	?warn(Unhandled),
 	{noreply,State}.
+
+handle_info({timeout,TRef,{fl_timeout,Speed}},State=#state{timer=TRef,timer_intv=S})->
+
+	CurrentOutputs=piface2:read_output(),
+	NewOutputs=CurrentOutputs bxor S,
+	Reply=piface2:write_output(NewOutputs),
+
+	erlang:start_timer(S,self(),{fl_timeout,S}),
+	{noreply,State};
 
 handle_info(Msg,State)->
 	Unhandled={unhandled_info,{msg,Msg}},
@@ -98,6 +121,18 @@ terminate(Reason,#state{})->
 % Utility
 %==============================================================================
 
+i_on(Port)->
+	CurrentOutputs=piface2:read_output(),
+	NewOutputs=CurrentOutputs bor (1 bsl (Port)),
+	piface2:write_output(NewOutputs).
 
+i_off(Port)->
+	CurrentOutputs=piface2:read_output(),
+	NewOutputs=CurrentOutputs band bnot (1 bsl (Port)),
+	piface2:write_output(NewOutputs).
 
+i_toggle(Port)->
+	CurrentOutputs=piface2:read_output(),
+	NewOutputs=CurrentOutputs bxor (1 bsl (Port)),
+	piface2:write_output(NewOutputs).
 
