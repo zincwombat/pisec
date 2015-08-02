9c2a21ea3c0a134a510c8ca05c5153f21b575692
zincwombat <mail@tonyhobbins.com>
2015-08-02 16:31:03 +1000

    enabled living room PIR sensor

--- priv/alarmconfig.erl
+++ priv/alarmconfig.erl
@@ -4,14 +4,14 @@
 	{1,test,"Test",true,0,control},
 	{2,front_door,"Front Door Sensor",true,1,sensor},
 	{3,rear_door,"Rear Door Sensor",true,1,sensor},
-	{4,hallway,"Hallway PIR Sensor",false,0,sensor},
-	{5,living,"Living Room PIR Sensor",false,0,sensor},
+	{4,hallway,"Hallway PIR Sensor",false,1,sensor},
+	{5,living,"Living Room PIR Sensor",true,1,sensor},
 	{6,input7,"Input 6",false,0,sensor},
 	{7,input8,"Input 7",false,0,sensor}
 ]}.
 {port_handler_history_size,	20}.
 {outputs,[
-	% format, {PortNum,desc,LongDesc,initialState,led|power}
+	% format, {PortNum,label,LongDesc,enabled,initialState,led|power}
 	{0,siren,"Siren (Relay 1)",true,off,power},
 	{1,relay1,"Relay 2",true,off,power},
 	{2,power_led,"Power On LED",true,off,led},
